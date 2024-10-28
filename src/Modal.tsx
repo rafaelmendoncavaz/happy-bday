eb8a17440e153607b62180ac3e4a674cf92e53f9
RAFAEL <rafaelmendoncavaz@hotmail.com>
2024-10-28 17:28:29 -0300

    fix vercel

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -39,12 +39,13 @@ export function Modal({ closeModal }: ModalProps) {
                 <div className="flex flex-col items-center px-2 py-5">
                     <iframe
                         title="Birthday Video"
-                        style="width: 560; height: 315;"
+                        width={560}
+                        height={315}
                         src="https://www.youtube.com/embed/jo-01q8Ko08?si=utd-OJRnMEUr0Aam"
                         title="YouTube video player"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerpolicy="strict-origin-when-cross-origin"
-                        allowfullscreen
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
                     ></iframe>
                 </div>
             </div>
